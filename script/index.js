39eb4c3b476523538634d295c1c3f0d02e5e6c0d
Sajmul <sajmul1427@gmail.com>
2024-10-10 19:22:09 +0600

    first touch

--- script/index.js
+++
@@ -1,5 +0,0 @@
-let year = document.getElementById('copy-year');
-
-let date = new Date();
-
-year.innerText = date.getFullYear();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*","./about/about.html"],
+  content: [],
   theme: {
     extend: {},
   },
